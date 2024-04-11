2cb8b235431aac80385234f5333651f851f55f55
suzie <puppychico0930@gmail.com>
2024-04-11 17:54:59 -0600

    scaffolding

---
+++ INTEX3/Models/AspNetRole.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace INTEX3.Models;
+
+public partial class AspNetRole
+{
+    public string Id { get; set; } = null!;
+
+    public string? Name { get; set; }
+
+    public string? NormalizedName { get; set; }
+
+    public string? ConcurrencyStamp { get; set; }
+}
